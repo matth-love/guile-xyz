5e3a9b723d721d196e7cf87e4e673485f1411381
matthew love <matthew.love@colorado.edu>
2021-03-12 15:29:39 -0800

    initial commit

---
+++ modules/xyz/infos.scm
@@ -0,0 +1,165 @@
+;;*-scheme-*
+;;; infos.scm
+;;
+;; Copyright (c) 2018 - 2021 Matthew Love <matthew.love@colorado.edu>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; Get some information about an xyz-data port/list.
+;;
+;;; Code:
+
+(define-module (xyz infos)
+  #:version (0 1 4)
+  #:use-module (ice-9 rdelim)
+  #:use-module (ice-9 regex)
+  #:use-module (xyz xyz)
+  #:export
+  (infos->region xyz->infos xyz-port->infos infos->list inf->infos))
+
+(define (infos->region infos)
+  "- Scheme Procedure: infos->region infos
+    Convert an infos-blob to a region-list."
+  (list (assq-ref infos 'xmin )
+	(assq-ref infos 'xmax )
+	(assq-ref infos 'ymin )
+	(assq-ref infos 'ymax )))
+
+(define (infos->list infos)
+  "- Scheme Procedure: infos->list infos
+    Convert an infos-bog to a list"
+  (if (not (null? infos))
+      (list
+       (assq-ref infos 'name)
+       (list
+        (assq-ref infos 'xmin)
+        (assq-ref infos 'xmax)
+        (assq-ref infos 'ymin)
+        (assq-ref infos 'ymax))
+       (list
+        (assq-ref infos 'zmin)
+        (assq-ref infos 'zmax)))))
+
+(define (xyz-make-infos name x1 y1 z1)
+  (acons 'name name
+	 (acons 'xmin x1
+		(acons 'xmax x1
+		       (acons 'ymin y1
+			      (acons 'ymax y1 
+				     (acons 'zmin z1
+					    (acons 'zmax z1
+						   (acons 'count 1 '())))))))))
+
+(define (xyz-set-infos! infos x1 y1 z1)
+  (let ((xmin (assq-ref infos 'xmin)) (xmax (assq-ref infos 'xmax))
+	(ymin (assq-ref infos 'ymin)) (ymax (assq-ref infos 'ymax))
+	(zmin (assq-ref infos 'zmin)) (zmax (assq-ref infos 'zmax)))
+    (if (< x1 xmin) 
+	(assq-set! infos 'xmin x1)
+	(if (> x1 xmax) 
+	    (assq-set! infos 'xmax x1)))
+    (if (< y1 ymin) 
+	(assq-set! infos 'ymin y1)
+	(if (> y1 ymax) 
+	    (assq-set! infos 'ymax y1)))
+    (if (< z1 zmin) 
+	(assq-set! infos 'zmin z1)
+	(if (> z1 zmax) 
+	    (assq-set! infos 'zmax z1))))
+  (assq-set! infos 'count (1+ (assq-ref infos 'count))))
+
+(define* (xyz->infos xyz-list #:optional (infos #f))
+  "- Scheme Procedure: xyz->infos xyz-list #:infos
+    Snarf an infos blob from an xyz-list."
+  (cond
+   ((null? xyz-list) infos)
+   ((not infos)
+    (xyz->infos (cdr xyz-list) 
+		(xyz-make-infos 
+		 "xyz-info"
+		 (list-ref (car xyz-list) 0) 
+		 (list-ref (car xyz-list) 1) 
+		 (list-ref (car xyz-list) 2))))
+   (else
+    (xyz->infos (cdr xyz-list) 
+		(xyz-set-infos! 
+		 infos
+		 (list-ref (car xyz-list) 0) 
+		 (list-ref (car xyz-list) 1) 
+		 (list-ref (car xyz-list) 2))))))
+
+(define* (xyz-port->infos #:optional 
+			  (port (current-input-port))
+			  (infos #f) 
+			  #:key (test #f))
+  "- Scheme Procedure: xyz-port->infos #:port #:infos #:test
+    Snarf an infos blob from an xyz-port."
+  (define xyz-infos
+    (lambda (xyz)
+      (if (or (not test) 
+	      (test xyz))
+	  (let ((x1 (vector-ref xyz 0)) 
+		(y1 (vector-ref xyz 1)) 
+		(z1 (vector-ref xyz 2)))
+	    (set! infos 
+		  (if infos
+		      (xyz-set-infos! infos x1 y1 z1)
+		      (xyz-make-infos (port-filename port) x1 y1 z1)))))))
+  (xyz->scm port 
+	    #:index "xyz" 
+	    #:list/vector #f 
+	    #:test-fun test 
+	    #:data-fun xyz-infos 
+	    #:loop-fun (lambda (a b) infos)))
+
+(define* (inf->infos info-port #:optional (close? #f) (infos '()))
+  "- Scheme Procedure: inf->infos info-port #:close? #:info
+    Convert an MBSystem .inf file to an infos blob"
+  (if (eof-object? (peek-char info-port)) 
+      (begin 
+	(if close? (close-port info-port))
+	infos)
+      (let ((info-line (read-line info-port)))
+	(let ((count (string-match "Number of Records:" info-line))
+	      (name (string-match "Swath Data File:" info-line))
+	      (lon (string-match "Minimum Longitude:" info-line))
+	      (lat (string-match "Minimum Latitude:" info-line))
+	      (dep (string-match "Minimum Depth:" info-line)))
+	  (cond
+	   ((regexp-match? name)
+	    (let ((nm
+		   (string-trim-both (match:suffix name))))
+	      (inf->infos info-port close? (acons 'name nm infos))))
+	   ((regexp-match? count)
+	    (let ((cnt
+		   (string-trim-both (match:suffix count))))
+	      (inf->infos info-port close? (acons 'count (string->number cnt) infos))))
+	   ((regexp-match? lon)
+	    (let ((lons
+		   (string-split (string-trim-both (match:suffix lon)) #\sp)))
+	      (inf->infos info-port close? (acons 'xmin (string->number (car lons)) (acons 'xmax (string->number (car (reverse lons))) infos)))))
+	   ((regexp-match? lat)
+	    (let ((lats
+		   (string-split (string-trim-both (match:suffix lat)) #\sp)))
+	      (inf->infos info-port close? (acons 'ymin (string->number (car lats)) (acons 'ymax (string->number (car (reverse lats))) infos)))))
+	   ((regexp-match? dep)
+	    (let ((deps
+		   (string-split (string-trim-both (match:suffix dep)) #\sp)))
+	      (inf->infos info-port close? (acons 'zmin (string->number (car deps)) (acons 'zmax (string->number (car (reverse deps))) infos)))))
+	   (else
+	    (inf->infos info-port close? infos)))))))
+
+;;; End
---
+++ modules/xyz/xyz.scm
@@ -0,0 +1,197 @@
+;;*-scheme-*
+;;; xyz.scm
+;;
+;; Copyright (c) 2018 - 2021 Matthew Love <matthew.love@colorado.edu>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; XYZ module influenced by nala ginrut's guile-csv, but uses (ice-9 rdelim) and has per/line delimiter detection for mixed files
+;; and is only concerned with numerical data from the csv.
+;;
+;; Maybe use hooks instead?
+;;
+;;; Code:
+
+(define-module (xyz xyz)
+  #:use-module (ice-9 rdelim)
+  #:export (xyz-exts xyz-read-line make-xyz-reader xyz-display xyz->scm xyz->port echo-port cat-port))
+
+(define xyz-exts
+  '(".xyz"))
+
+(define* (xyz-read-line 
+	  #:optional 
+	  (port (current-input-port))
+	  #:key 
+	  (data-fun (lambda (data-line) 
+		      (map string->number data-line))))
+  "- Scheme Procedure: xyz-read-line [ port #:data-fun ]"
+  (define %known-delimiters 
+    '(#\tab #\sp #\, #\ht #\; #\| #\/ #\:))
+  (define string-split/delims 
+    (lambda (str)
+     (let lp ((delims %known-delimiters))
+	(let ((l (string-split str (car delims))))
+	  (cond
+	   ((not (null? (cdr l))) l)
+	   ((not (null? (cdr delims)))
+	    (lp (cdr delims)))
+	   (else '()))))))
+  (let ((this-char (peek-char port)))
+    (cond 
+     ((eof-object? this-char)
+      this-char)
+     (else
+      (let ((data-line (map string-trim-both (string-split/delims (string-trim-both (read-line port))))))
+	(data-fun data-line))))))
+
+(define* (make-xyz-reader 
+	  #:optional 
+	  (port (current-input-port))
+	  #:key 
+	  (data-fun (lambda (data-list) 
+		      (map string->number data-list)))
+	  (loop-fun (lambda (current-line out-data)
+		      (cons current-line out-data))))
+  "- Scheme Procedure: make-xyz-reader [ port #:data-fun ]"
+  (lambda (port)
+    (let lp ((this-port port) (out-data '()))
+      (let ((this-line (xyz-read-line port #:data-fun data-fun)))
+	(if (eof-object? this-line) out-data
+	    (lp this-port (loop-fun this-line out-data)))))))
+
+;; Read in xyz data from port. Return a list of xy* point(s).
+;; Use #:index to set the column index; i.e. "xyz" if the columns of the xyz data are x,y,z; "yxz" if xy are flipped, etc.
+;; If #:header is #t will discard the first record.
+;; Set #:list/vector to either #t (list) or #f (vector) to output either a list of point-lists '((x y z) (x y z) ... (x y z)) or a list
+;; of vector-points '(#(x y z) #(x y z) ... #(x y z)).
+;; The #:data-fun key sets how to process a xyz-record.
+;; The #:loop-fun key sets what to do with the xyz-record (default is to cons them all together)
+;; The #:test-fun key sets a boolean test on each xyz-record and will only return the xyz-point if it passes the test. 
+;; e.g. (lambda (xyz) (< (cadddr xyz) 0)) to return only points which have a z value greater than 0.
+;; (xyz->scm (open-file "test.yxz" "r") 
+;;    #:list/vector #f
+;;    #:data-fun (lambda (xyz) (map string->number xyz)) 
+;;    #:loop-fun (lambda (xyz xyzs) (cons xyz xyzs))
+;;    #:index "yxz"
+;; or:
+;; (xyz->scm (open-file "test.xyz" "r"))
+(define* (xyz->scm #:optional 
+		   (port (current-input-port)) 
+		   #:key 
+		   (header #f) 
+		   (list/vector #t)
+		   (data-fun #f) 
+		   (loop-fun #f) 
+		   (test-fun #f) 
+		   (index "xyz"))
+  "- Scheme Procedure: xyz->scm [ port #:header #:test-fun #:index ]"
+  (define string->number/nan
+    (lambda (xyz-value)
+      (let ((xyz-number (string->number xyz-value)))
+	(if (not xyz-number) (nan) xyz-number))))
+  (define xyz-line->xyz-list
+    (lambda (xyz-line)
+      (let ((xpos (string-index index #\x))
+	    (ypos (string-index index #\y))
+	    (zpos (string-index index #\z))
+	    (xyz-len (length xyz-line)))
+	(if (null? xyz-line) (nan)
+	    ((if list/vector list vector)
+	     (if xpos (if (> xyz-len xpos) (string->number/nan (list-ref xyz-line xpos)) (nan)) (nan))
+	     (if ypos (if (> xyz-len ypos) (string->number/nan (list-ref xyz-line ypos)) (nan)) (nan))
+	     (if zpos (if (> xyz-len zpos) (string->number/nan (list-ref xyz-line zpos)) (nan)) (nan)))))))
+  (define this-data-fun
+    (if data-fun 
+	(lambda (xyz-line) 
+	  (data-fun (xyz-line->xyz-list xyz-line)))
+	xyz-line->xyz-list))
+  (define this-loop-fun
+    (if loop-fun loop-fun
+	(lambda (xyz xyzs)
+	  (if (or (not test-fun)
+		  (test-fun xyz))
+	      (cons xyz xyzs) xyzs))))
+  (define reader 
+    (make-xyz-reader port 
+		     #:data-fun this-data-fun 
+		     #:loop-fun this-loop-fun))
+  (let ((xyz (reader port)))
+    (if header (cdr xyz) xyz)))
+
+;; Read in xyz data from port and display it to oport.
+;; Set the #:test-fun key to test the points before displaying them to oport.
+(define* (xyz->port #:optional 
+		    (port (current-input-port)) 
+		    (oport (current-output-port))
+		    #:key
+		    (verbose #f)
+		    (infos #f)
+		    (test-fun #f)
+		    (weight #f)
+		    (index "xyz"))
+  "- Scheme Procedure: xyz->port [ input-port output-port #:verbose #:infos #:test-fun #:weight #:index ]"
+  (let ((count 0))
+    (xyz->scm port 
+	      #:data-fun 
+	      (lambda (xyz) 
+		(if (or (not test-fun) (test-fun xyz))
+		    (begin
+		      (set! count (+ count 1))
+		      (xyz-display xyz oport #:weight weight))))
+	      #:loop-fun (lambda (a b) b)
+	      #:index index)
+    (if verbose
+	(format (current-error-port) "xyz: ~a points read from ~a~%" count (if infos (assq-ref infos 'name) (port-filename port))))))
+
+(define* (xyz-display xyz
+		      #:optional (port (current-output-port))
+		      #:key 
+		      (delim #\sp)
+		      (weight #f))
+  "- Scheme Procedure: xyz-display [ port #:delim #:weight ]"
+  (if (pair? xyz)
+      (if (pair? (car xyz))
+	  (let loop ((these-xyz xyz))
+	    (when (pair? these-xyz)
+		  (xyz-display (car these-xyz) port)
+		  (loop (cdr these-xyz))))
+	  (begin
+	    (let loop ((this-point xyz))
+	      (display (car this-point) port)
+	      (when (not (null? (cdr this-point)))
+		    (display delim port)
+		    (loop (cdr this-point))))
+	    (if weight (begin
+			 (display delim port)
+			 (display weight port)))
+	    (newline port)))))
+
+(define* (echo-port port #:optional (outport (current-output-port)))
+  (if (not (eof-object? (peek-char port)))
+      (begin
+	(display (read-line port 'concat) outport)
+	(echo-port port outport))))
+
+(define* (cat-port port 
+		   #:optional (oport (current-output-port))
+		   #:key (weight 1))
+  (if (not (eof-object? (peek-char port)))
+      (let ((port-line (read-line port)))
+	(format oport "~a ~a~%" port-line weight)
+	(cat-port port oport #:weight weight))))
+
+;;; End
